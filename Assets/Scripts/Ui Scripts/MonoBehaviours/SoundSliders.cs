e9516a0713150186c0a04880be7d7595a7e5525a
Kris <pg17kris.vfs.com@gmail.com>
2020-05-17 15:28:01 -0700

    Added "MainMenu" and "Gameplay" scenes, sound slider scripts
    volume sliders are functional but are not currently hooked up to any WWise channels

---
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/SoundSliders.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSliders : MonoBehaviour
+{
+    //NOTE: Slider values operate on a scale of 0 to 1
+    [SerializeField] public Slider volumeSlider;
+    [SerializeField] public float defaultVolume = 0.5f;
+    [SerializeField] public string WWiseChannel;
+    public float newVolume;
+
+    private void Start()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+    public void SetVolumeSlider(string channel)
+    {
+        newVolume = volumeSlider.value;
+        AkSoundEngine.SetRTPCValue($"{WWiseChannel}", newVolume);
+    }
+}
