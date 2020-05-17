7fc6769beb49b4bbef8a81009c2fd338d2ec5f80
Jmaster <Jmaster9999@gmail.com>
2020-05-17 15:57:09 -0700

    Created Tool Controller and Tool baseclass

---
+++ Assets/Scripts/Systems/Tools/Core/Tool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public abstract class Tool : ScriptableObject
+{
+    public delegate void ToolEvent(ToolController owner);
+
+    public ToolEvent OnActivateEvent;
+    public ToolEvent OnDeactivateEvent;
+    public ToolEvent WhileActiveEvent;
+
+    protected string ToolName = "";
+    public abstract void OnActivate();
+    public abstract void OnDeactivate();
+    public abstract void OnWhileActive();
+
+    public void Activate(ToolController owner)
+    {
+        if (OnActivateEvent != null) OnActivateEvent(owner);
+    }
+
+    public void Deactivate(ToolController owner)
+    {
+        if (OnDeactivateEvent != null) OnDeactivateEvent(owner);
+    }
+
+    public void WhileActive(ToolController owner)
+    {
+        if (WhileActiveEvent != null) WhileActiveEvent(owner);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Systems/Tools/ToolController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolController : MonoBehaviour
+{
+
+    [SerializeField] private List<Tool> EquiptTools = new List<Tool>();
+
+    private Tool CurrentTool = null;
+
+    private bool CurrentToolIsActive = false;
+
+    public void SwitchTool(int ToolIndex)
+    {
+        Debug.Assert(ToolIndex < EquiptTools.Count && ToolIndex >= 0);
+        if (CurrentToolIsActive) return; //dont switch tools if the current tool is in use
+        CurrentTool = EquiptTools[ToolIndex];
+    }
+
+    public void ActivateTool()
+    {
+        if (CurrentTool == null) return;
+        CurrentToolIsActive = true;
+        CurrentTool.Activate(this);
+    }
+
+    public void DeactivateTool()
+    {
+        if (CurrentTool == null) return;
+        CurrentToolIsActive = false;
+        CurrentTool.Deactivate(this);
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentToolIsActive)
+        {
+            CurrentTool.WhileActive(this);
+        }
+    }
+}
