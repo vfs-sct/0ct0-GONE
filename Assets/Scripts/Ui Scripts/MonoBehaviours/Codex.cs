acdccd5b8d9649e1cdbe4bc34320165a100cafa4
Kris <pg17kris.vfs.com@gmail.com>
2020-05-18 15:24:37 -0700

    setting up for codex functionality

--- Assets/Scripts/Ui Scripts/MonoBehaviours/Codex.cs
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/Codex.cs
@@ -1,5 +1,5 @@
 ﻿//using System.Collections;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
@@ -8,6 +8,31 @@ using TMPro;
 public class Codex : MonoBehaviour
 {
     [SerializeField] GameObject PausePrefab;
+    [SerializeField] VerticalLayoutGroup content;
+    [SerializeField] GameObject defaultHeader;
+    [SerializeField] GameObject defaultButton;
+    [SerializeField] TextMeshProUGUI entryTitleText;
+    [SerializeField] TextMeshProUGUI entryBodyText;
+
+
+    Dictionary<string, string> logEntries = new Dictionary<string, string>
+    {
+        {"Log One", "Log one text"},
+        {"Log Two", "Log two text"},
+        {"Log Three", "Log three text"},
+        {"Log Four", "Log four text"},
+        {"Log Five", "Log five text"},
+        {"Log Six", "Log six text"},
+        {"Log Seven", "Log seven text"},
+        {"Log Eight", "Log eight text"},
+    };
+
+    public Button[] tutorialEntries;
+
+    private void Start()
+    {
+        
+    }
 
     public void OnEsc(InputValue value)
     {
