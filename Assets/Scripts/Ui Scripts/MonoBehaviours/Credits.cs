c8759d3791672a8b009ae0fd65ba9ac34ba90396
Kris <pg17kris.vfs.com@gmail.com>
2020-05-18 14:14:45 -0700

    credits autogenerate

--- Assets/Scripts/Ui Scripts/MonoBehaviours/Credits.cs
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/Credits.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class Credits : MonoBehaviour
 {
     [SerializeField] GameObject MainMenuPrefab;
+    [SerializeField] TextMeshProUGUI content;
+
+    public string[] credits = new string[] 
+    { 
+        "Josh Paquette",
+        "Jesse Rougeau",
+        "Kristin Ruff-Frederickson",
+        "Evan Landry",
+        "Andrew Icardi",
+        "Roger Crusafon-Pont",
+    };
+
+    private void Start()
+    {
+        foreach (var credit in credits)
+        {
+            content.SetText(content.text + $"\n{credit}");
+        }
+    }
+
     public void OnEsc(InputValue value)
     {
         SwitchViewTo(MainMenuPrefab);
