3fec88a72d19a7c0c4fb5e7035a2600767f2911f
Kris <pg17kris.vfs.com@gmail.com>
2020-05-17 18:31:44 -0700

    gamma slider functional, settings persist between scenes

--- Assets/Scripts/Ui Scripts/MonoBehaviours/GammaSlider.cs
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/GammaSlider.cs
@@ -5,15 +5,16 @@ public class GammaSlider : MonoBehaviour
 {
     //NOTE: Slider values operate on a scale of 0 to 1
     [SerializeField] public Slider gammaSlider;
-    [SerializeField] public float defaultGamma = 0.5f;
+    
     private float newGamma;
 
     private void Start()
     {
-        gammaSlider.value = defaultGamma;
+        gammaSlider.value = Shader.GetGlobalFloat("gamma");
     }
     public void SetGammaSlider()
     {
         newGamma = gammaSlider.value;
+        Shader.SetGlobalFloat("gamma", newGamma);
     }
 }
---
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/UIAwake.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAwake : MonoBehaviour
+{
+    [SerializeField] public float gammaDefault = 2.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var camera = Camera.main;
+
+        foreach (var canvas in GameObject.FindObjectsOfType<Canvas>())
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            canvas.worldCamera = camera;
+        }
+
+        camera.gameObject.AddComponent<PostProcessing>().material = Resources.Load<Material>("GammaMaterial");
+
+        if (Shader.GetGlobalFloat("gamma") == 0)
+        {
+            Shader.SetGlobalFloat("gamma", gammaDefault);
+        }
+
+    }
+}
