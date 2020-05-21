61ef9804410ab8ad88eb2751bc41498bc7a697fd
Kris <pg17kris.vfs.com@gmail.com>
2020-05-21 16:24:58 -0700

    stubbing in resource panel
    gamma slider has a wider spectrum of contrast now
    resource assets have player-facing strings

--- Assets/Scripts/Systems/Resources/Core/Resource.cs
+++ Assets/Scripts/Systems/Resources/Core/Resource.cs
@@ -29,6 +29,7 @@ public class Resource : ScriptableObject
     [SerializeField] private float Minimum = 0 ;
     [SerializeField] private float DefaultValue = 0;
     [SerializeField] private float Maximum = 100;
+    [SerializeField] public string DisplayName = "";
 
     Dictionary<ResourceInventory,ResourceData_Internal> Data = new Dictionary<ResourceInventory, ResourceData_Internal>();
 
---
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/ResourcePanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class ResourcePanel : MonoBehaviour
+{
+    [SerializeField] public ResourceInventory playerInventory = null;
+    [SerializeField] private Resource[] resources = null;
+    [SerializeField] public VerticalLayoutGroup contentGroup = null;
+    [SerializeField] public GameObject defaultText = null;
+
+    Dictionary<Resource, GameObject> resourceList;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(var resource in resources)
+        {
+            var currentAmount = playerInventory.GetResource(resource);
+            var newLine = Instantiate(defaultText);
+
+            newLine.transform.SetParent(contentGroup.transform);
+
+            newLine.GetComponent<TextMeshProUGUI>().SetText(resource.DisplayName + ": " + currentAmount);
+
+            //save an association between the resource and the new entry we've created so we can pick out
+            //specific resources to update later
+            resourceList.Add(resource, newLine);
+        }
+    }
+
+    void UpdateResourceAmount(Resource resource, float newAmount)
+    {
+        var updateLine = resourceList[resource];
+
+        updateLine.GetComponent<TextMeshProUGUI>().SetText(resource.DisplayName + ": " + newAmount);
+    }
+}
