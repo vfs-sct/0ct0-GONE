fc2b8db23484c86cf0b3ad5003a89268473a6a31
Kris <pg17kris.vfs.com@gmail.com>
2020-05-21 19:01:02 -0700

    committing to go check develop branch

---
+++ Assets/Scripts/Ui Scripts/MonoBehaviours/ObjectivePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivePanel : MonoBehaviour
+{
+
+
+    Dictionary<string, string> narrativeObjectives = new Dictionary<string, string>
+    {
+        {"Objective01", "Repair the destroyed ship"},
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
