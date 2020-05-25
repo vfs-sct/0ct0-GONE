55d28fe048301f5340c5156eb99f2576f8eedc43
Evan Landry <evan.landryna@gmail.com>
2020-05-25 13:33:37 -0700

    - Added new Wwise Version

---
+++ WWise_OctoGone_SD95_20200521/WWise_OctoGone_SD95_20200521/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,147 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ASTEROID_COLLISION = 494181607U;
+        static const AkUniqueID ASTEROID_MOVING = 376042431U;
+        static const AkUniqueID ASTEROID_WARNING = 1329011239U;
+        static const AkUniqueID COMMUNICATIONS_ARRAY = 2894345104U;
+        static const AkUniqueID COMPLETE_OBJECTIVE = 3263450676U;
+        static const AkUniqueID CRAFTING_FAILURE = 2185317076U;
+        static const AkUniqueID CRAFTING_SUCCESS = 507489755U;
+        static const AkUniqueID CREDITS = 2201105581U;
+        static const AkUniqueID CREDITS_BUTTON_BACK = 1158635394U;
+        static const AkUniqueID DAMAGE = 1786804762U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID ELECTRICAL_SPARK_WARNING = 3147538972U;
+        static const AkUniqueID ELECTRICITY = 2917121896U;
+        static const AkUniqueID ENDING_CINEMATIC = 3221693214U;
+        static const AkUniqueID ENV_01_START = 1545542569U;
+        static const AkUniqueID ENV_01_STOP = 3085466819U;
+        static const AkUniqueID EXPLOSION = 13776098U;
+        static const AkUniqueID FIRE = 2678880713U;
+        static const AkUniqueID FUEL_RESTORATION = 678929340U;
+        static const AkUniqueID GAME_OVER = 1432716332U;
+        static const AkUniqueID GAS = 579662040U;
+        static const AkUniqueID GAS_WARNING = 3096260197U;
+        static const AkUniqueID IMPACT = 3257506471U;
+        static const AkUniqueID IN_GAME_THEME = 4011822115U;
+        static const AkUniqueID LOW_FUEL = 4123804492U;
+        static const AkUniqueID LOW_SUPPLY = 973306129U;
+        static const AkUniqueID MAIN_MENU = 2005704188U;
+        static const AkUniqueID MAINMENU_ALL_BUTTON_HOVER = 2586944717U;
+        static const AkUniqueID MAINMENU_BUTTON_CREDITS = 1462477445U;
+        static const AkUniqueID MAINMENU_BUTTON_OPTIONS = 3362106049U;
+        static const AkUniqueID MAINMENU_BUTTON_PLAY = 574491607U;
+        static const AkUniqueID MAINMENU_BUTTON_QUIT = 2978081472U;
+        static const AkUniqueID MAINMENU_TRANSITION = 575977795U;
+        static const AkUniqueID METAL_COLLISION = 1341439083U;
+        static const AkUniqueID NOT_ENOUGH_RESOURCES = 853073649U;
+        static const AkUniqueID OCTO_DAMAGE_METAL = 1805563388U;
+        static const AkUniqueID OCTO_DAMAGE_ROCK = 3567146260U;
+        static const AkUniqueID OCTO_DOWNTHRUSTERS = 1967397267U;
+        static const AkUniqueID OCTO_GOOGLUE = 269952755U;
+        static const AkUniqueID OCTO_GRAB = 2765733061U;
+        static const AkUniqueID OCTO_HORITHRUSTERS = 702990819U;
+        static const AkUniqueID OCTO_LASERCUTTER = 1507525885U;
+        static const AkUniqueID OCTO_MOVING = 2120872765U;
+        static const AkUniqueID OCTO_TETHER = 1089170435U;
+        static const AkUniqueID OCTO_UPTHRUSTERS = 761624014U;
+        static const AkUniqueID OPTIONS_AUDIO_BUTTON_BACK = 4155334453U;
+        static const AkUniqueID OPTIONS_AUDIO_VOLUME_MASTER = 4073980316U;
+        static const AkUniqueID OPTIONS_AUDIO_VOLUME_MUSIC = 1189984679U;
+        static const AkUniqueID OPTIONS_AUDIO_VOLUME_SFX = 1505081371U;
+        static const AkUniqueID OPTIONS_BUTTON_AUDIO = 3719034323U;
+        static const AkUniqueID OPTIONS_BUTTON_BACK = 4161904774U;
+        static const AkUniqueID OPTIONS_BUTTON_VIDEO = 937090132U;
+        static const AkUniqueID OPTIONS_VIDEO_BUTTON_BACK = 1009666018U;
+        static const AkUniqueID OPTIONS_VIDEO_BUTTON_RESOLUTION = 176897593U;
+        static const AkUniqueID OPTIONS_VIDEO_GAMMA = 2275707507U;
+        static const AkUniqueID OPTIONS_VIDEO_RESOLUTION_BUTTON_APPLY = 694886012U;
+        static const AkUniqueID OPTIONS_VIDEO_RESOLUTION_BUTTON_REVERT = 3593654154U;
+        static const AkUniqueID QUIT_BUTTON_NO = 3372377107U;
+        static const AkUniqueID QUIT_BUTTON_YES = 2275034407U;
+        static const AkUniqueID REMEMBERING_OAKLEY = 3812427620U;
+        static const AkUniqueID SATELLITE_COMPONENT = 1580704404U;
+        static const AkUniqueID SATELLITE_CRAFTING = 2901737241U;
+        static const AkUniqueID SATELLITE_FOUNDRY = 3028867906U;
+        static const AkUniqueID SATELLITE_FUEL_STATION = 62827578U;
+        static const AkUniqueID SATELLITE_IDLE = 1040610669U;
+        static const AkUniqueID SATELLITE_INCINERATOR = 3336320773U;
+        static const AkUniqueID SATELLITE_RADAR = 1479001789U;
+        static const AkUniqueID SYSTEM_SOUNDS = 2357789153U;
+        static const AkUniqueID THREAT_THEME = 2900252007U;
+        static const AkUniqueID THRUSTERS = 1944582317U;
+        static const AkUniqueID TITLE_SCREEN = 3853285476U;
+        static const AkUniqueID VOICE_LOG_01 = 2903534464U;
+        static const AkUniqueID VOICE_LOG_02 = 2903534467U;
+        static const AkUniqueID VOICE_LOG_03 = 2903534466U;
+        static const AkUniqueID VOICE_LOG_04 = 2903534469U;
+        static const AkUniqueID VOICE_LOG_05 = 2903534468U;
+        static const AkUniqueID VOICE_LOG_06 = 2903534471U;
+        static const AkUniqueID VOICE_LOG_07 = 2903534470U;
+        static const AkUniqueID VOICE_LOG_08 = 2903534473U;
+        static const AkUniqueID WHITE_NOISE_SPACESHIP = 1630925514U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
+        static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
+        static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID INTENSITY = 2470328564U;
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+        static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID WWISE_OCTO_MAIN = 2856818978U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUS_MAIN = 3031508118U;
+        static const AkUniqueID SFX_MAIN = 3023356346U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
