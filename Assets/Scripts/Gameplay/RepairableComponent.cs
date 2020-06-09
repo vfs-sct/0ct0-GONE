5fb51a06258e8bb46cec049e2c7293562972799a
Jmaster <Jmaster9999@gmail.com>
2020-06-09 13:08:26 -0700

    Created Health and Repair components
    - Created a health component that has delegates for events based on adding,removing and delta health
    - Created a repair component that will be used by the repair tool
    - Created a DoRepair function that will try to repair the component and return true if it was successful

---
+++ Assets/Scripts/Gameplay/RepairableComponent.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairableComponent : MonoBehaviour
+{
+    public delegate void RepairEvent(RepairableComponent parent);
+
+
+
+    [System.Serializable]
+    public struct RepairRequirements
+    {
+        public Resource resource;
+        public int requiredAmount;
+        public int currentAmount;
+        public RepairRequirements(Resource Rs,int RA)
+        {
+            resource = Rs;
+            requiredAmount = RA;
+            currentAmount = 0;
+        }
+         public RepairRequirements(Resource Rs,int RA,int CA)
+        {
+            resource = Rs;
+            requiredAmount = RA;
+            currentAmount = CA;
+        }
+    }
+    [SerializeField] private List<RepairRequirements> RequiredResources = new List<RepairRequirements>();
+
+
+    private RepairEvent OnFullRepair;
+    private bool _Repaired = false;
+    public bool Repaired{get =>_Repaired;}
+
+    public void AddRepairResource(int index,int amount)
+    {
+        RequiredResources[index] = new RepairRequirements(RequiredResources[index].resource,RequiredResources[index].requiredAmount,RequiredResources[index].currentAmount+amount);
+    }
+
+
+    public bool DoRepair(ResourceInventory RepairInventory)
+    {
+        if (_Repaired) return false;
+        bool RepairCycle = false;
+        int MetRequirements = 0;
+
+        float ResourceStorage = 0;
+        for (int i = 0; i < RequiredResources.Count; i++)
+        {
+            if (RequiredResources[i].currentAmount < RequiredResources[i].requiredAmount)
+            {
+                ResourceStorage = RepairInventory.GetResource(RequiredResources[i].resource);
+                if (ResourceStorage > 0)
+                {
+                    RepairCycle = true;
+                    RepairInventory.RemoveResource(RequiredResources[i].resource,1);
+                    AddRepairResource(i,1);
+                }
+            }
+            else
+            {
+                MetRequirements ++;
+            }
+        }
+        _Repaired = 
+        if (_Repaired) OnFullRepair(this);
+        return RepairCycle;
+    }    
+
+
+}
---
+++ Assets/Scripts/Systems/Tools/Tools/RepairTool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairTool : Tool
+{
+    protected override bool ActivateCondition(ToolController owner, GameObject target)
+    {
+        return true;
+    }
+
+    protected override bool DeactivateCondition(ToolController owner, GameObject target)
+    {
+        return true;
+    }
+
+    protected override bool LoopCondition(ToolController owner, GameObject target)
+    {
+        return true;
+    }
+
+    protected override void OnActivate(ToolController owner, GameObject target)
+    {
+
+    }
+
+    protected override void OnDeactivate(ToolController owner, GameObject target)
+    {
+        
+    }
+
+    protected override void OnDeselect(ToolController owner)
+    {
+        
+    }
+
+    protected override void OnSelect(ToolController owner)
+    {
+        
+    }
+
+    protected override void OnWhileActive(ToolController owner, GameObject target)
+    {
+        
+    }
+}
