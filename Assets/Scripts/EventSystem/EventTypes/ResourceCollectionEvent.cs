4efc985548a0fce3c803558d6a8f49e578d15589
Kris <pg17kris.vfs.com@gmail.com>
2020-06-11 14:29:05 -0700

    updated readme, added some of jesse's t1 recipes to crafting menu

---
+++ Assets/Scripts/EventSystem/EventTypes/ResourceCollectionEvent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Systems/Events/Resource Collection Event")]
+public class ResourceCollectionEvent : Event
+{
+    public bool EventTrigger = false;
+    [SerializeField] Resource[] CollectResource;
+    [SerializeField] int[] ResourceAmount;
+    [SerializeField] private ResourceModule ResourceController = null;
+
+    public override bool Condition(GameObject target)
+    {
+        return EventTrigger;
+    }
+
+    protected override void Effect(GameObject target)
+    {
+        //no effect, this uses unity events for any effects
+    }
+}
---
+++ Assets/Scripts/EventSystem/EventTypes/ResourceCraftingEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Systems/Events/Resource Crafting Event")]
+public class ResourceCraftingEvent : Event
+{
+    public bool EventTrigger = false;
+    [SerializeField] Resource[] CollectResource;
+    [SerializeField] int[] ResourceAmount;
+
+    public override bool Condition(GameObject target)
+    {
+        return EventTrigger;
+    }
+
+    protected override void Effect(GameObject target)
+    {
+        //no effect, this uses unity events for any effects
+    }
+}
