0cdde0bb21c3ddb5e4433da9ee58f3a38c870e0d
Evan Landry <evan.landryna@gmail.com>
2020-06-11 17:32:18 -0700

    - Grab Tool Base
    Base template Grab tool.
    Going to see if I can set up a grabber component or something that interacts with the rigid bodies of objects that use such component

---
+++ Assets/Scripts/Systems/Tools/Tools/GrabTool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Systems/Tools/Grabber")]
+public class GrabTool : Tool
+{
+    protected override bool ActivateCondition(ToolController owner, GameObject target)
+    {
+        return true;
+    }
+
+    protected override bool DeactivateCondition(ToolController owner, GameObject target)
+    {
+        return true;
+    }
+
+    protected override bool LoopCondition(ToolController owner, GameObject target)
+    {
+        return false;
+    }
+
+    protected override void OnActivate(ToolController owner, GameObject target)
+    {
+        Debug.Log("Grabber Activated");
+    }
+
+    protected override void OnDeactivate(ToolController owner, GameObject target)
+    {
+        Debug.Log("Grabber Deactivated");
+    }
+
+    protected override void OnDeselect(ToolController owner)
+    {
+        Debug.Log("Grabber Detected");
+    }
+
+    protected override void OnSelect(ToolController owner)
+    {
+        Debug.Log("Grabber DeSelected");
+    }
+
+    protected override void OnWhileActive(ToolController owner, GameObject target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
