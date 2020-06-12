d8593c369b6dd4fc17477d114516e4c27916f131
Evan Landry <evan.landryna@gmail.com>
2020-06-11 23:37:22 -0700

    - Added ThrusterAudioPlayer.cs and impact handler scripts
    - Scripts will be handling the RTCP level of the thrusters, and tagged the player with the "Player" tag to ensure the salvage wasn't triggering metal impacts over and over.

---
+++ Assets/Scripts/AudioScripts/Player Audio/ThrusterAudioPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterAudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.Event PlayThrusterAudio;
+    [SerializeField] private AK.Wwise.Event StopThrusterAudio;
+    [SerializeField] private AK.Wwise.RTPC ThrusterImpulseRTPC;
+
+    [SerializeField] private SpaceMovement Movement;
+
+    private void Start()
+    {
+        PlayThrusterAudio.Post(gameObject);
+    }
+
+    private void Update()
+    {
+        ThrusterImpulseRTPC.SetGlobalValue(Mathf.Abs(Movement.Throttle.x + Movement.Throttle.y + Movement.Throttle.z) / 3);
+        Debug.Log(ThrusterImpulseRTPC.GetGlobalValue());
+        Debug.Log("==== Setting RTCP ====");
+    }
+
+    private void OnDestroy()
+    {
+        StopThrusterAudio.Post(gameObject);
+    }
+}
