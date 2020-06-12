b14d00911ac27f3e2a18c0ba4f6c5860ca4a343e
Evan Landry <evan.landryna@gmail.com>
2020-06-11 22:36:31 -0700

    - Added Collision Sound to Salvage
    - Gave Player a player tag, added Metal sounds to salvage.

---
+++ Assets/Scripts/AudioScripts/CollisionAudioPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.Event CollisionAudioEvent;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CollisionAudioEvent.Post(gameObject);
+        } 
+    }
+}
