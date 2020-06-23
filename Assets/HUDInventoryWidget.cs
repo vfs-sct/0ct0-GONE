458a8a00070aaf9c02bc8cacba2b4972a0464f01
Kris <pg17kris.vfs.com@gmail.com>
2020-06-22 20:24:46 -0700

    autogenerate resource dials

--- Assets/HUDInventoryWidget.cs
+++ Assets/HUDInventoryWidget.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class HUDInventoryWidget : MonoBehaviour
 {
@@ -8,16 +9,40 @@ public class HUDInventoryWidget : MonoBehaviour
     [SerializeField] HorizontalLayoutGroup buttonPanel = null;
     [SerializeField] GameObject defaultDial = null;
 
-    private ResourceInventory playerInventory;
+    [SerializeField] private Resource[] resources = null;
+    
+    private Dictionary<Resource, Image> updateFill = new Dictionary<Resource, Image>();
+
+    private InventoryController bucketInventory;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        playerInventory = UIRoot.GetPlayer().GetComponent<ResourceInventory>();
+        bucketInventory = UIRoot.GetPlayer().GetComponent<InventoryController>();
+
+        foreach(var resource in resources)
+        {
+            GenerateDial(resource);
+        }
+    }
+
+    private void GenerateDial(Resource resource)
+    {
+        var newDial = Instantiate(defaultDial);
+        newDial.transform.SetParent(buttonPanel.transform);
+        var fillImage = newDial.GetComponent<GetObjectsDial>().GetFillImage();
+        fillImage.color = resource.ResourceColor;
+        newDial.GetComponent<GetObjectsDial>().GetText().SetText(resource.DisplayName);
+        updateFill.Add(resource, fillImage);
+        fillImage.fillAmount = bucketInventory.GetFillAmount(resource);
+        Debug.Log(bucketInventory.GetFillAmount(resource));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(var kvp in updateFill)
+        {
+            kvp.Value.fillAmount = bucketInventory.GetFillAmount(kvp.Key);
+        }
     }
 }
