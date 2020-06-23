25f8824818a1f486cad3206b7428fb3b69508c4e
Kris <pg17kris.vfs.com@gmail.com>
2020-06-22 18:34:11 -0700

    inventory box setup

---
+++ Assets/HUDInventoryWidget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUDInventoryWidget : MonoBehaviour
+{
+    [SerializeField] UIAwake UIRoot = null;
+    [SerializeField] HorizontalLayoutGroup buttonPanel = null;
+    [SerializeField] GameObject defaultDial = null;
+
+    private ResourceInventory playerInventory;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInventory = UIRoot.GetPlayer().GetComponent<ResourceInventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
