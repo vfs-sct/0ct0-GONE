a625aca8c0bd568e15b5c72993b81dd01962c226
Evan Landry <evan.landryna@gmail.com>
2020-06-24 03:13:54 -0700

    - Stash for tomorrow morning
    -> Added Asteroid Scripts
    -> Added Asteroid Field Game Object
    -> BUG: Spawn time isnt taken into consideration when Player enters the field.

---
+++ Assets/Scripts/Gameplay/Asteroids/AsteroidSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public float SpawnTimer = 200f;
+    public float SpawnHeight;
+    public GameObject Asteroid;
+
+    private float timer = 0f;
+    private bool isSpawning = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("==== Player Entered ThreatField ===="); 
+            isSpawning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("==== Player Exited ThreatField ====");
+            isSpawning = false;
+        }
+        Debug.Log(isSpawning);
+    }
+
+    private void Update()
+    {
+        if (isSpawning == true)
+        {
+            SpawnAsteroid();
+        }
+        else 
+        {
+            return;
+        }
+    }
+
+    private void SpawnAsteroid()
+    {
+        Debug.Log("==== Spawning Asteroids ====");
+        
+        if(timer < SpawnTimer)
+        {
+            GameObject newAsteroid = Instantiate(Asteroid);
+            newAsteroid.transform.position = transform.position + new Vector3(0, Random.Range(-SpawnHeight,SpawnHeight), 0);
+            Destroy(newAsteroid, 15f);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
