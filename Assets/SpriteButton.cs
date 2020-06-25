6f82fd8a5a72f32b54a2c1b7a7012ecb91c474be
Kris <pg17kris.vfs.com@gmail.com>
2020-06-25 12:40:19 -0700

    script for ignoring button image alpha

---
+++ Assets/SpriteButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SpriteButton : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.01f;
+    }
+}
