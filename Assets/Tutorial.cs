c78af26f37d29d35fe13ff08226e897d0d474410
Kris <pg17kris.vfs.com@gmail.com>
2020-06-27 20:12:08 -0700

    added refueling event
    teaches player about refueling

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] GameObject[] tutorialPrompts;
+    public int currentPrompt = 0;
+
+    public void FirstPrompt()
+    {
+        tutorialPrompts[0].SetActive(true);
+    }
+
+    public void NextPrompt()
+    {
+        tutorialPrompts[currentPrompt].SetActive(false);
+        currentPrompt++;
+        if(tutorialPrompts[currentPrompt] != null)
+        {
+            tutorialPrompts[currentPrompt].SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
