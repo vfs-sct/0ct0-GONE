656eebb52482db71b5562d251a4875b0b742851a
Evan Landry <evan.landryna@gmail.com>
2020-06-30 10:37:53 -0700

    Edited PlayButtonSound

--- Assets/Scripts/AudioScripts/Buttons/PlayButtonSound.cs
+++ Assets/Scripts/AudioScripts/Buttons/PlayButtonSound.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayButtonSound : MonoBehaviour
 {
+    // Handle the button clicks
     public void OnClick()
     {
-        AkSoundEngine.PostEvent ("MainMenu_Button_Play",gameObject);
+        AkSoundEngine.PostEvent("MainMenu_Button_Play",gameObject);
+    }
+
+    // Handles Mouse hovers
+    private void OnMouseOver()
+    {
+        AkSoundEngine.PostEvent("MainMenu_All_Button_Hover", gameObject);
     }
 
     public void OnClickPlayDialogue()
