ddb8830ac702ebd389494b62cf89ba5d34d18aa3
Evan Landry <evan.landryna@gmail.com>
2020-06-30 18:54:45 -0700

    - Added a State Manager for Audio stuff

---
+++ Assets/Scripts/AudioScripts/PlayerColliders/AkStateManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AkStateManager : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.State[] akStates;
+    [SerializeField] private string[] validTags;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isValidTag(other.tag))
+        {
+            foreach (var akState in akStates)
+            {
+                akState.SetValue();
+            }
+        }
+    }
+
+    private bool isValidTag(string tag)
+    {  
+        foreach (var validTag in validTags)
+        {
+            // Check if there are any valid tags inside the array.
+            if(validTag == tag)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
