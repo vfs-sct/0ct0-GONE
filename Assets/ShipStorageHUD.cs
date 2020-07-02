7caf04298650f915b81c295e4a3a4bc2c9a6389e
Kris <pg17kris.vfs.com@gmail.com>
2020-07-01 19:04:04 -0700

    adding storage display to crafting screen (nonfunctional)

---
+++ Assets/ShipStorageHUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShipStorageHUD : MonoBehaviour
+{
+    //storage owner is the ship whose inventory we want to display
+    [SerializeField] ResourceInventory storageOwner = null;
+    [SerializeField] HorizontalLayoutGroup dialLayout = null;
+    [SerializeField] GameObject dial = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
