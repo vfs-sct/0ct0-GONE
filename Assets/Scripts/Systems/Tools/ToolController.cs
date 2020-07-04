78921f78c48a0d6f99c387c67842b3c1c30556e1
Kris <pg17kris.vfs.com@gmail.com>
2020-07-03 21:51:48 -0700

    bug fix

--- Assets/Scripts/Systems/Tools/ToolController.cs
+++ Assets/Scripts/Systems/Tools/ToolController.cs
@@ -105,13 +105,11 @@ public class ToolController : MonoBehaviour
     {
         if (CurrentToolIsActive)
         {
-            /*
             if (!CurrentTool.WhileActive(this,_Target))
             {
                 DeactiveTool_Internal();
                 return;
             }
-            */
             CurrentTool.WhileActive(this,_Target);
         }
     }
