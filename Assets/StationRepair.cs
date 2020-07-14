f9176c43dab8f8f39b8024321c9072b35f673759
Kris <pg17kris.vfs.com@gmail.com>
2020-07-14 00:08:29 -0700

    repair component screen

--- Assets/StationRepair.cs
+++ Assets/StationRepair.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class StationRepair : MonoBehaviour
 {
+    [SerializeField] GameFrameworkManager GameManager = null;
+    [SerializeField] GameObject HUDPrefab = null;
+    [SerializeField] GameObject ComponentBox = null;
+    [SerializeField] HorizontalLayoutGroup[] rows = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,28 @@ public class StationRepair : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //crafting screen can either be closed with ESC or the hotkey to open it (or clicking the close button on the panel)
+    public void OnEsc(InputValue value)
+    {
+        Close();
+    }
+
+    public void OnCraftHotkey(InputValue value)
+    {
+        Close();
+    }
+
+    public void Close()
+    {
+        GameManager.UnPause();
+        SwitchViewTo(HUDPrefab);
+    }
+
+    public void SwitchViewTo(GameObject newPanel)
+    {
+        newPanel.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
