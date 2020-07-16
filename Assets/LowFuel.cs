007c5c67983bab61e069c4901f9d37a32126edfd
Kris <pg17kris.vfs.com@gmail.com>
2020-07-15 20:25:25 -0700

    set up for damage/low fuel flash

---
+++ Assets/LowFuel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class LowFuel : MonoBehaviour
+{
+
+    private bool fadeOut = false;
+    [SerializeField] private FlashWhileActive flashScript = null;
+
+    private void Start()
+    {
+        flashScript.enabled = false;
+    }
+
+    public void TurnOn()
+    {
+        this.gameObject.SetActive(true);
+        flashScript.enabled = true;
+    }
+
+    public void TurnOff()
+    {
+        fadeOut = true;
+        flashScript.enabled = false;
+    }
+
+    public void FadeOut()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(fadeOut)
+       {
+            FadeOut();
+       }
+    }
+}
