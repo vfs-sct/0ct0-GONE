ad0b79a00d4b643a261c191c930914c0d0f19bee
Kris <pg17kris.vfs.com@gmail.com>
2020-07-20 15:06:36 -0700

    spaceship hud indicator!
    using WIP assets so arrows very ugly

---
+++ Assets/OffscreenIndicator.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OffscreenIndicator : MonoBehaviour
+{
+    [SerializeField] private GameObject spaceStation = null;
+    [SerializeField] private Image indicator = null;
+    [SerializeField] private Image shipHealthBar = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShipHealth();
+    }
+
+    public void ShipHealth()
+    {
+        Vector3 screenpos = Camera.main.WorldToScreenPoint(spaceStation.transform.position);
+
+        if(screenpos.z > 0 && 
+            screenpos.x > 0 && 
+            screenpos.x < Screen.width && 
+            screenpos.y > 0 && 
+            screenpos.y < Screen.height)
+        {
+            shipHealthBar.gameObject.SetActive(true);
+            indicator.gameObject.SetActive(false);
+            shipHealthBar.transform.position = screenpos;
+        }
+        else
+        {
+            shipHealthBar.gameObject.SetActive(false);
+            indicator.gameObject.SetActive(true);
+
+            if(screenpos.z<0)
+            {
+                screenpos *= -1;
+            }
+
+            Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) / 2;
+
+            screenpos -= screenCenter;
+
+            float angle = Mathf.Atan2(screenpos.y, screenpos.x);
+            angle -= 90 * Mathf.Deg2Rad;
+
+            float cos = Mathf.Cos(angle);
+            float sin = -Mathf.Sin(angle);
+
+            screenpos = screenCenter + new Vector3(sin * 150, cos * 150, 0);
+
+            float m = cos / sin;
+
+            Vector3 screenBounds = screenCenter * 0.9f;
+
+            //check up/down
+            if(cos>0)
+            {
+                screenpos = new Vector3(screenBounds.y / m, screenBounds.y, 0);
+            }
+            else
+            {
+                screenpos = new Vector3(-screenBounds.y / m, -screenBounds.y, 0);
+            }
+            //if out of bounds, get correct side
+            if(screenpos.x > screenBounds.x)
+            {
+                screenpos = new Vector3(screenBounds.x, screenBounds.x * m, 0);
+            }
+            else if (screenpos.x <-screenBounds.x)
+            {
+                screenpos = new Vector3(-screenBounds.x, -screenBounds.x * m, 0);
+            }
+
+            screenpos += screenCenter;
+
+            indicator.transform.position = screenpos;
+            indicator.transform.localRotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+        }
+    }
+}
