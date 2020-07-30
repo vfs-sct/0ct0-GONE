b0b23145fb289545b88faaeb063fc104759815b6
Jmaster <Jmaster9999@gmail.com>
2020-07-29 17:19:55 -0700

    WIP Instanced Rendering

---
+++ Assets/Scripts/Systems/InstancedRendering/InstancedMeshReplacer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstancedMeshReplacer : MonoBehaviour
+{
+    [SerializeField] private InstancedRenderingModule RenderController;
+    private void Awake()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Systems/InstancedRendering/InstancedRenderingModule.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstancedRenderingModule : Module
+{
+
+    struct IMeshData
+    {
+        public Mesh mesh;
+        public int subMeshIndex;
+        public Material material;
+        public MaterialPropertyBlock properties;
+        public bool recieveShadows;
+        public int layer;
+
+        public IMeshData(bool active, Mesh mesh, int subMeshIndex, Material material, MaterialPropertyBlock properties, bool recieveShadows, int layer)
+        {
+            this.mesh = mesh;
+            this.subMeshIndex = subMeshIndex;
+            this.material = material;
+            this.properties = properties;
+            this.recieveShadows = recieveShadows;
+            this.layer = layer;
+        }
+
+      /*  public override bool Equals(object obj) //not sure how well this works;
+        {
+            return obj is IMeshData data &&
+                   active == data.active &&
+                   EqualityComparer<Mesh>.Default.Equals(mesh, data.mesh) &&
+                   subMeshIndex == data.subMeshIndex &&
+                   EqualityComparer<Material>.Default.Equals(material, data.material) &&
+                   EqualityComparer<MaterialPropertyBlock>.Default.Equals(properties, data.properties) &&
+                   recieveShadows == data.recieveShadows &&
+                   layer == data.layer;
+        }*/
+    }
+
+    struct IRenderData
+    {
+        
+
+
+
+    }
+
+
+    private Dictionary<IMeshData,HashSet<GameObject>> RenderData = new Dictionary<IMeshData, HashSet<GameObject>>();
+
+    private Dictionary<GameObject,Matrix4x4 matrix> RenderData = new Dictionary<GameObject,Matrix4x4 matrix>();
+
+
+
+
+
+
+
+
+    public override void Reset()
+    {
+        
+    }
+}
