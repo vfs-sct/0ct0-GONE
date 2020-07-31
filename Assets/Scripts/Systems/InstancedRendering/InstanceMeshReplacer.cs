9fb4d62ce1a5bef6b80f2507b75920f3c1b034fd
Jmaster <Jmaster9999@gmail.com>
2020-07-30 17:22:05 -0700

    Added Irender module and mesh replacer

---
+++ Assets/Scripts/Systems/InstancedRendering/InstanceMeshReplacer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceMeshReplacer : MonoBehaviour
+{
+
+    [SerializeField] private InstancedRenderingModule IRenderingManager;
+
+    [SerializeField] private Mesh LinkedMesh;
+
+    [SerializeField] private MeshRenderer LinkedRenderer;
+
+    private void Awake()
+    {
+        IRenderingManager.AddInstancedMesh(gameObject,InstancedRenderingModule.GenerateIMeshData(gameObject,LinkedMesh,LinkedRenderer));
+        Destroy(LinkedMesh);
+        Destroy(LinkedRenderer);
+    }
+
+}
---
+++ Assets/Scripts/Systems/InstancedRendering/InstancedRenderingModule.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Systems/InstancedRendering/IRender Module")]
+public class InstancedRenderingModule : Module
+{
+
+    public struct IMeshData
+    {
+        public Mesh mesh;
+        public int subMeshIndex;
+        public Material material;
+        public MaterialPropertyBlock properties;
+        public bool recieveShadows;
+        public int layer;
+
+        public IMeshData(Mesh mesh, int subMeshIndex, Material material, MaterialPropertyBlock properties, bool recieveShadows, int layer)
+        {
+            this.mesh = mesh;
+            this.subMeshIndex = subMeshIndex;
+            this.material = material;
+            this.properties = properties;
+            this.recieveShadows = recieveShadows;
+            this.layer = layer;
+        }
+
+        public override bool Equals(object obj) //not sure how well this works;
+        {
+            return obj is IMeshData data &&
+                   EqualityComparer<Mesh>.Default.Equals(mesh, data.mesh) &&
+                   subMeshIndex == data.subMeshIndex &&
+                   EqualityComparer<Material>.Default.Equals(material, data.material) &&
+                   EqualityComparer<MaterialPropertyBlock>.Default.Equals(properties, data.properties) &&
+                   recieveShadows == data.recieveShadows &&
+                   layer == data.layer;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 233193393;
+            hashCode = hashCode * -1521134295 + EqualityComparer<Mesh>.Default.GetHashCode(mesh);
+            hashCode = hashCode * -1521134295 + subMeshIndex.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Material>.Default.GetHashCode(material);
+            hashCode = hashCode * -1521134295 + EqualityComparer<MaterialPropertyBlock>.Default.GetHashCode(properties);
+            hashCode = hashCode * -1521134295 + recieveShadows.GetHashCode();
+            hashCode = hashCode * -1521134295 + layer.GetHashCode();
+            return hashCode;
+        }
+    }
+    private Dictionary<IMeshData,HashSet<GameObject>> RenderData = new Dictionary<IMeshData, HashSet<GameObject>>();
+
+
+    public static IMeshData GenerateIMeshData(GameObject Owner,Mesh mesh,MeshRenderer meshRenderer)
+    {
+        IMeshData temp = new IMeshData(mesh,meshRenderer.subMeshStartIndex,meshRenderer.material,new MaterialPropertyBlock(),false,Owner.layer);
+        return temp;
+        throw new System.Exception(Owner+": Mesh Data could not be generated!");
+    }
+
+
+    public void AddInstancedMesh(GameObject Owner,IMeshData MeshData)
+    {
+        bool foundData = false;
+        foreach (var item in RenderData)
+        {
+            if (item.Key.Equals(MeshData))
+            {
+                RenderData[item.Key].Add(Owner);
+                foundData = true;
+                break;
+            }
+        }
+        if (!foundData)
+        {
+            HashSet<GameObject> temp = new HashSet<GameObject>();
+            temp.Add(Owner);
+            RenderData.Add(MeshData,temp);
+        }
+    }
+
+
+
+
+
+
+
+    public override void Reset()
+    {
+        
+    }
+}
