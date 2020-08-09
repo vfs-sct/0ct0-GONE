7832bd591f734444e59a2aba1966f4072ebe9bc9
Kris <pg17kris.vfs.com@gmail.com>
2020-08-09 16:04:16 -0700

    found a disconnected reference

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -293,8 +293,8 @@ public class Player : MonoBehaviour
         else if(root.tag == "Repairable")
         {
             RepairableScreenTooltip.SetActive(true);
-            CraftingTooltip.SetActive(false);
-            RefuellingTooltip.SetActive(false);
+            CraftingTooltip.SetActive(true);
+            RefuellingTooltip.SetActive(true);
         }
         else if(root.tag == "NaniteFactory")
         {
